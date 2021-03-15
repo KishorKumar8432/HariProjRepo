aa754cbc9d041f3415c2291894635b5649c13de9
Kishor <kishorpatel1606@gmail.com>
2021-03-15 13:44:31 +0530

    first

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+//import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		//ChromeDriver driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		
+		//FirefoxDriver driver =new FirefoxDriver();
+		
+		//InternetExplorerDriver driver = new InternetExplorerDriver();
+		
+		//EdgeDriver driver = new EdgeDriver();
+		
+		//String url = "https://www.rediff.com/";
+		//driver.get(url);
+	}
+
+}
